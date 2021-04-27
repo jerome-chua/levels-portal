146becfac7611cbd8c8c720b8ef1139989f6a1d6
Jchu4 <jerome.chuame@gmail.com>
2021-04-27 17:55:05 +0800

    Add ErrorBoundary component

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // Log any errors from component within  boundary.
+  componentDidCatch(err, errInfo) {
+    console.error(err, errInfo);
+    this.setState({ hasError: true });
+  }
+
+  // Render error message.
+  render() {
+    if (this.state.hasError) {
+      return <h1>Error occurred.</h1>;
+    }
+
+    // If no error return contents within boundary.
+    return this.props.children;
+  }
+}
