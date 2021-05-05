37a81b8f402c1c49dbd319fac2b947d7c356fbac
Jchu4 <jerome.chuame@gmail.com>
2021-05-05 14:55:38 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,30 +1,40 @@
-// Import dependencies.
-import express from 'express';
 import cookieParser from 'cookie-parser';
+import express from 'express';
 import methodOverride from 'method-override';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from './webpack_conf/webpack.dev.js';
 import bindRoutes from './routes.mjs';
 
-const app = express(); // Initialise Express instance.
+// Initialise Express instance
+const app = express();
+// Set the Express view engine to expect EJS templates
+app.set('view engine', 'ejs');
+// Bind cookie parser middleware to parse cookies in requests
+app.use(cookieParser());
+// Bind Express middleware to parse request bodies for POST requests
+app.use(express.urlencoded({ extended: false }));
+// Bind Express middleware to parse JSON request bodies
+app.use(express.json());
+// Bind method override middleware to parse PUT and DELETE requests sent as POST requests
+app.use(methodOverride('_method'));
+// Expose the files stored in the public folder
+app.use(express.static('public'));
+// Expose the files stored in the distribution folder
+app.use(express.static('dist'));
 
-app.set('view engine', 'ejs'); // Set the Express view engine to expect EJS templates.
-app.use(cookieParser()); // Bind cookie parser middleware to parse cookies in requests.
-app.use(express.urlencoded({ extended: false })); // Bind Express middleware to parse request bodies for POST requests.
-app.use(express.json()); // Bind Express middleware to parse JSON request bodies.
-app.use(methodOverride('_method')); // Bind method override middleware to parse PUT/DELETE requests sent as POST requests.
-app.use(express.static('public')); // Expose files stored in the public folder.
-app.use(express.static('dist')); // Expose files stored in the distribution folder.
-
-// Set up Webpack in dev env.
+// Set up Webpack in dev env
 const env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
+
+  const { default: webpack } = await import('webpack')
+  const { default: webpackDevMiddleware } =  await import('webpack-dev-middleware');
+  const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
+  const { default: webpackConfig } = await import('./webpack_conf/webpack.dev.js');
+
   const compiler = webpack(webpackConfig);
+
   app.use(webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
-    writeToDisk: (filePath) => /\.html$/.test(filePath), // HTML only.
+    // html only
+    writeToDisk: (filePath) => /\.html$/.test(filePath),
   }));
   app.use(webpackHotMiddleware(compiler, {
     log: false,
@@ -33,9 +43,9 @@ if (env === 'development') {
   }));
 }
 
-// Bind route definitions to the Express application.
+// Bind route definitions to the Express application
 bindRoutes(app);
 
-// Set Express to listen on given port.
+// Set Express to listen on the given port
 const PORT = process.env.PORT || 3004;
 app.listen(PORT);
